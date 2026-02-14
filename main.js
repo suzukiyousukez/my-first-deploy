b7ea4659a418e425952df39061c62495c21683e0
suzukiyousukez <40996896+suzukiyousukez@users.noreply.github.com>
2026-02-14 11:43:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: "#2d2d2d",
-  physics: {
-    default: "arcade",
-    arcade: { debug: false }
-  },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  },
-  scene: {
-    preload,
-    create,
-    update
-  }
-};
-
-const game = new Phaser.Game(config);
-
-let player;
-let cursors;
-let pointer;
-
-function preload() {}
+let debugText;
 
 function create() {
   const graphics = this.add.graphics();
@@ -39,31 +13,16 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
 
   pointer = this.input.activePointer;
+
+  debugText = this.add.text(10, 10, "no touch", { color: "#ffffff" });
 }
 
 function update() {
-  const speed = 200;
   player.setVelocity(0);
 
-  // キーボード操作
-  if (cursors.left.isDown) player.setVelocityX(-speed);
-  else if (cursors.right.isDown) player.setVelocityX(speed);
-
-  if (cursors.up.isDown) player.setVelocityY(-speed);
-  else if (cursors.down.isDown) player.setVelocityY(speed);
-
-  // タッチ操作
   if (pointer.isDown) {
-    const angle = Phaser.Math.Angle.Between(
-      player.x,
-      player.y,
-      pointer.worldX,
-      pointer.worldY
-    );
-
-    player.setVelocity(
-      Math.cos(angle) * speed,
-      Math.sin(angle) * speed
-    );
+    debugText.setText("touching");
+  } else {
+    debugText.setText("no touch");
   }
 }
